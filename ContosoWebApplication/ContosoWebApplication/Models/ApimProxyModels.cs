a3857424b31a64fe845559e4f78d2c2a8aec8771
habibkam <habibk@outlook.com>
2019-05-31 10:44:48 -0700

    initial code

---
+++ ContosoWebApplication/ContosoWebApplication/Models/ApimProxyModels.cs
@@ -0,0 +1,37 @@
+﻿namespace ContosoWebApplication.Models
+{
+    using System.Runtime.Serialization;
+
+    [DataContract]
+    public class ApimSubscriptionCreateOrUpdateContract
+    {
+        [DataMember(Name = "ownerId")]
+        public string OwnerId { get; set; }
+        [DataMember(Name = "scope")]
+        public string Scope { get; set; }
+        [DataMember(Name = "state")]
+        public string State { get; set; }
+    }
+
+    [DataContract]
+    public class ApimUserCreateOrUpdateContact
+    {
+        [DataMember(Name = "firstName")]
+        public string FirstName { get; set; }
+        [DataMember(Name = "lastName")]
+        public string LastName { get; set; }
+        [DataMember(Name = "email")]
+        public string Email { get; set; }
+        [DataMember(Name = "password")]
+        public string Password { get; set; }
+        [DataMember(Name = "state")]
+        public string State { get; set; }
+    }
+
+    [DataContract]
+    public class ApimSsoUrlResult
+    {
+        [DataMember(Name = "value")]
+        public string Value { get; set; }
+    }
+}
